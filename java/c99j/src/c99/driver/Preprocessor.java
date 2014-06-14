00bd48e08d0d5ef108925831f435a86161e618e4
Tzvetan Mikov <tmikov@gmail.com>
2014-06-13 23:45:21 -0700

    Preprocessor: add simple command line parameter handling
    Debug output is controlled via the command line

--- java/c99j/src/c99/driver/Preprocessor.java
+++ java/c99j/src/c99/driver/Preprocessor.java
@@ -14,9 +14,48 @@ public static void main ( String[] args )
 {
   try
   {
+    boolean cpp = true;
+    boolean toks = false;
+    String fileName = null;
+
+    for ( String arg : args )
+    {
+      if ("--toks".equals( arg ))
+      {
+        toks = true;
+        cpp = false;
+      }
+      else if ("--no-toks".equals( arg ))
+        toks = false;
+      else if ("--cpp".equals( arg ))
+        cpp = true;
+      else if ("--no-cpp".equals( arg ))
+        cpp = false;
+      else if (arg.startsWith( "-"))
+      {
+        System.err.println( "**fatal: unknown command line option '"+arg +"'" );
+        System.exit(1);
+      }
+      else
+      {
+        if (fileName != null)
+        {
+          System.err.println( "**fatal: More than one input filename specified" );
+          System.exit(1);
+        }
+        fileName = arg;
+      }
+    }
+
+    if (fileName == null)
+    {
+      System.err.println( "**fatal: No input filename specified" );
+      System.exit(1);
+    }
+
     DummyErrorReporter reporter = new DummyErrorReporter();
     SymTable symTable = new SymTable();
-    Prepr pp = new Prepr( reporter, args[0], new FileInputStream( args[0] ), symTable );
+    Prepr pp = new Prepr( reporter, fileName, new FileInputStream( fileName ), symTable );
 
     String lastFile = "";
     int lastLine = -1;
@@ -25,7 +64,7 @@ public static void main ( String[] args )
     {
       if (!tok.getFileName().equals( lastFile ))
       {
-        System.out.format(  "\n# %d %s\n", tok.getLine1(), tok.getFileName() );
+        if (cpp) System.out.format(  "\n# %d %s\n", tok.getLine1(), tok.getFileName() );
         lastLine = tok.getLine1();
       }
       else if (tok.getLine1() != lastLine)
@@ -33,19 +72,18 @@ public static void main ( String[] args )
         if (tok.getLine1() - lastLine <= 10)
         {
           do
-            System.out.println();
+            if (cpp) System.out.println();
           while (++lastLine < tok.getLine1());
         }
         else
-          System.out.format( "\n# %d\n", tok.getLine1() );
+          if (cpp) System.out.format( "\n# %d\n", tok.getLine1() );
         lastLine = tok.getLine1();
       }
 
       lastFile = tok.getFileName();
 
-      tok.output( System.out );
-      if (false)
-        System.err.println( tok );
+      if (cpp) tok.output( System.out );
+      if (toks) System.out.println( tok );
     }
   }
   catch (Exception e)
