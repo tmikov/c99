dd73442ce0810911c42bbba248717381b7889a40
Tzvetan Mikov <tmikov@gmail.com>
2014-07-24 19:30:41 -0700

    Types: implement integerPromotion() and usualArithmeticConversions()

--- java/c99j/src/c99/Types.java
+++ java/c99j/src/c99/Types.java
@@ -2,8 +2,9 @@ package c99;
 
 import java.util.HashMap;
 
-public interface Types
+public class Types
 {
+private Types () {}
 
 public static final int CHAR_BITS = 8;
 public static final int SHORT_BITS = 16;
@@ -49,6 +50,7 @@ public static enum TypeSpec
 
   public final boolean arithmetic;
   public final boolean floating;
+  public final boolean integer;
   public final boolean signed;
   public final int width;
   public final long longMask;
@@ -61,6 +63,7 @@ public static enum TypeSpec
   {
     this.arithmetic = false;
     this.floating = false;
+    this.integer = false;
     this.signed = false;
     this.width = 0;
     this.longMask = 0;
@@ -75,6 +78,7 @@ public static enum TypeSpec
     assert width > 0;
     this.arithmetic = true;
     this.floating = true;
+    this.integer = false;
     this.signed = true;
     this.width = width;
     this.longMask = this.width < 64 ? (1L << this.width) - 1 : ~0L;
@@ -89,6 +93,7 @@ public static enum TypeSpec
     assert width > 0;
     this.arithmetic = true;
     this.floating = false;
+    this.integer = true;
     this.signed = signed;
     this.width = width;
     this.longMask = this.width < 64 ? (1L << this.width) - 1 : ~0L;
@@ -106,6 +111,78 @@ public static enum TypeSpec
     this.minReal = 0;
     this.maxReal = 0;
   }
+
+  TypeSpec toUnsigned ()
+  {
+    assert this.integer;
+    return signed ? values()[this.ordinal() + 1] : this;
+  }
+}
+
+public static TypeSpec integerPromotion ( TypeSpec spec )
+{
+  // 6.3.1.1 [2]
+  assert spec.integer;
+  if (spec.ordinal() < TypeSpec.SINT.ordinal())
+    return (spec.width - (spec.signed?1:0) <= TypeSpec.SINT.width) ? TypeSpec.SINT : TypeSpec.UINT;
+  else
+    return spec;
+}
+
+public static TypeSpec usualArithmeticConversions ( TypeSpec s0, TypeSpec s1 )
+{
+  // 6.3.1.8
+
+  Types.TypeSpec greaterRank = s0.ordinal() > s1.ordinal() ? s0 : s1;
+
+  if (greaterRank.floating)
+    return greaterRank;
+
+  s0 = integerPromotion( s0 );
+  s1 = integerPromotion( s1 );
+
+  // If both operands have the same type, then no further conversion is needed.
+  if (s0 == s1)
+    return s0;
+
+  TypeSpec lesserRank;
+  if (s0.ordinal() > s1.ordinal())
+  {
+    greaterRank = s0;
+    lesserRank = s1;
+  }
+  else
+  {
+    greaterRank = s1;
+    lesserRank = s0;
+  }
+
+  // Otherwise, if both operands have signed integer types or both have unsigned
+  // integer types, the operand with the type of lesser integer conversion rank is
+  // converted to the type of the operand with greater rank.
+  //
+  // Otherwise, if the operand that has unsigned integer type has rank greater or
+  // equal to the rank of the type of the other operand, then the operand with
+  // signed integer type is converted to the type of the operand with unsigned
+  // integer type.
+  //
+  // Otherwise, if the type of the operand with signed integer type can represent
+  // all of the values of the type of the operand with unsigned integer type, then
+  // the operand with unsigned integer type is converted to the type of the
+  // operand with signed integer type.
+  if (s0.signed == s1.signed ||
+      !greaterRank.signed ||
+      greaterRank.width-1 >= lesserRank.width)
+  {
+    return greaterRank;
+  }
+
+  // Otherwise, both operands are converted to the unsigned integer type
+  // corresponding to the type of the operand with signed integer type.
+  assert greaterRank.signed;
+  assert !lesserRank.signed;
+
+  return greaterRank.toUnsigned();
 }
 
 public static class Qual
