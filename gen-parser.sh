a0600717945b69d33897f14d9f03279c4c7a31ad
Tzvetan Mikov <tmikov@gmail.com>
2014-07-29 14:50:21 -0700

    gen-parser.sh: a script to invoke bison

---
+++ gen-parser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+vis=""
+if [ "$1" == "-x" ]; then
+  vis="-x"
+  shift
+fi
+
+
+ydir="java/c99j/y"
+javadir="java/c99j/src/c99/parser"
+skeldir="bison"
+
+bison $vis -Wall -S $skeldir/lalr1.java $ydir/CParser.y -o $javadir/CParser.java
+
+if [ -n "$vis" ]; then
+  mv $javadir/CParser.xml $ydir/
+
+  datadir=`bison --print-datadir`
+  xsltproc $datadir/xslt/xml2xhtml.xsl $ydir/CParser.xml > $ydir/CParser.html
+fi
